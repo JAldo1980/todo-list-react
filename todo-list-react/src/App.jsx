4659651929181d238138019fd17479b077526ecc
JAldo1980 <jamesalderman1980@gmail.com>
2023-10-16 17:45:45 +0100

    added states to various function expressions

--- todo-list-react/src/App.jsx
+++ todo-list-react/src/App.jsx
@@ -1,24 +1,31 @@
 import React, { useState, useTransition } from "react";
 import "./App.css";
 import DarkLightToggle from "./DarkLightToggle";
-import CreateTodo from "./CreateTodo";
+import Main from "./Main";
 
 function App() {
   const [isDark, setIsDark] = useState(false);
 
   const toggleTheme = () => {
-    setIsDark((prevIsDark) => !prevIsDark);
+    setIsDark((prevState) => !prevState);
+    if (isDark) {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
   };
 
   return (
     <>
-      <div className="body">
-        <header>
-          <h1>Todo List</h1>
-          <p>(Built with REACT.js)</p>
-        </header>
-        <DarkLightToggle isDark={isDark} toggleTheme={toggleTheme} />
-        <CreateTodo />
+      <div className={`app ${isDark ? "dark" : "light"}`}>
+        <div className="body">
+          <header>
+            <h1>Todo List</h1>
+            <p>(Built with REACT.js)</p>
+          </header>
+          <DarkLightToggle isDark={isDark} toggleTheme={toggleTheme} />
+          <Main />
+        </div>
       </div>
     </>
   );
